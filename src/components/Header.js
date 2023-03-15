9c4a40d14d49c2452fe12504091a0fbc2b9186c5
Erkut Cakmak <cakmak.erkut1@gmail.com>
2023-03-15 13:49:43 -0400

    lab complete

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React, {useState} from "react";
+
+function Header({onDarkModeClick, isDarkMode}) {
+
+    const handleClick = () => onDarkModeClick();
+
+    return(
+    <header>
+        <h2>Shopster</h2>
+        <button onClick={handleClick}>
+          {isDarkMode ? "Dark" : "Light"} Mode
+        </button>
+    </header>
+    );
+}
+
+export default Header;
